5b2719bd0fcd0e39af4d7f65a12c893cfe5666d5
D4M4EVER <53546851+D4M4EVER@users.noreply.github.com>
2022-01-21 15:55:58 -0800

    Update pve_post_install.sh

--- misc/pve_post_install.sh
+++ misc/pve_post_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "\e[1;33m This script will Disable the Enterprise Repo, Add & Enable the No-Subscription Repo, 
+echo -e "\e[0;34 This script will Disable the Enterprise Repo, Add & Enable the No-Subscription Repo, 
 Add & Disable Test Repo (repo's can be enabled/disabled via the UI in Repositories) 
 and attempt the No-Nag fix. PVE7 ONLY \e[0m"
 while true; do
@@ -21,11 +21,11 @@ deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 EOF
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
 apt --reinstall install proxmox-widget-toolkit &>/dev/null
-echo -e "\e[1;33m No-Subscription Repo Updated....\e[0m"
+echo -e "\e[0;34 No-Subscription Repo Updated....\e[0m"
 apt update &>/dev/null
-echo -e "\e[1;33m Updating Proxmox....Please wait....\e[0m"
+echo -e "\e[0;34 Updating Proxmox....Please wait....\e[0m"
 apt -qqy upgrade &>/dev/null
-echo -e "\e[1;33m Update Completed\e[0m"
+echo -e "\e[0;34 Update Completed\e[0m"
 
 
 # bash <(curl -s https://raw.githubusercontent.com/D4M4EVER/Proxmox_Scripts/main/misc/pve_post_install.sh )
