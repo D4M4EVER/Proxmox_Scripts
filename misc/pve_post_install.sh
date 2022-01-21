4ab194b6813551ba6a44019779071d306d76e5f5
D4M4EVER <53546851+D4M4EVER@users.noreply.github.com>
2022-01-21 14:59:58 -0800

    Update pve_post_install.sh

--- misc/pve_post_install.sh
+++ misc/pve_post_install.sh
@@ -21,7 +21,11 @@ deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 EOF
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
 apt --reinstall install proxmox-widget-toolkit &>/dev/null
-echo -e "\e[1;33m Finished....Please Update Proxmox \e[0m"
+echo -e "\e[1;33m No-Subscription Repo Updated....\e[0m"
+apt update &>/dev/null
+apt -qqy upgrade &>/dev/null
+echo -e "\e[1;33m Updating Proxmox....\e[0m"
+
 
 # bash -c "$(wget -qLO - https://raw.githubusercontent.com/D4M4EVER/Proxmox_Scripts/main/misc/pve_post_install.sh)"
 # Scripts based off [tteck] Proxmox Helpers, https://github.com/tteck/Proxmox
